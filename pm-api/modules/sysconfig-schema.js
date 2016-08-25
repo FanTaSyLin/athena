d80102b335c95d21b71c9c7c61beeb196bc364f0
FanTaSyLin <fanlin19830908@hotmail.com>
2016-08-25 15:05:31 +0800

    1

--- pm-api/modules/sysconfig-schema.js
+++ pm-api/modules/sysconfig-schema.js
@@ -3,25 +3,50 @@
  */
 
 var mongoose = require('mongoose');
-var DptGroupSchema = require('./departmentgroup-schema.js');
 var Schema = mongoose.Schema;
 
-var SysConifgSchema = new Schema({
-    name: {type: String},
+var SysConfigSchema = new Schema({
+    name: {type: String, unique: true},
+    /*部门分组*/
+    departmentGroups: [{
+        id: {type: Number, unique: true}, /*编号*/
+        name: {type: String}, /*名称*/
+    }],
     /*部门*/
     departments: [{
-        id: {type: Number}, /*编号*/
+        id: {type: Number, unique: true}, /*编号*/
         name: {type: String}, /*名称*/
         group: {type: Number} /*所属分组*/
     }],
-    /*部门分组*/
-    departmentGroups: [DptGroupSchema],
     /*权限模板*/
     authorityModels: [{
-        id: {type: Number},/*编号*/
-        name: {type: String},/*名称*/
+        id: {type: Number, unique: true}, /*编号*/
+        name: {type: String}, /*名称*/
         authority: [Boolean] /*权限*/
     }]
 });
 
-module.exports = mongoose.model('SysConifg', SysConifgSchema);
+SysConfigSchema.statics.getDptGroupItem = function (id, name) {
+    return {
+        id: id,
+        name: name
+    };
+};
+
+SysConfigSchema.statics.getDptItem = function (id, name, groupID) {
+    return {
+        id: id,
+        name: name,
+        group: groupID
+    };
+};
+
+SysConfigSchema.statics.getAuthorityModel = function (id, name, authority) {
+    return {
+        id: id,
+        name: name,
+        authority: authority
+    };
+};
+
+module.exports = mongoose.model('SysConfig', SysConfigSchema);
