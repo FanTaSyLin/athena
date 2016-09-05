f23c3016cd31582fc41f76f23647aadd17e47a2c
FanTaSyLin <fanlin19830908@hotmail.com>
2016-09-05 17:11:17 +0800

    1

--- pm-app/pm.js
+++ pm-app/pm.js
@@ -28,7 +28,6 @@ app.use(express.static("./app"));
 app.use(bodyParser.json());
 app.use(require('cookie-parser')());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, 'app')));
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
