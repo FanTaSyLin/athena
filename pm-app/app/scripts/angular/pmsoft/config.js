06a775cb408bbdf7287353cd82ef838e77981b01
FanTaSyLin <fanlin19830908@hotmail.com>
2016-09-09 19:32:31 +0800

    // Disable IE ajax request caching start         。。。。 // Disable IE ajax request caching end
    解决浏览器get请求缓存的问题

--- pm-app/app/scripts/angular/pmsoft/config.js
+++ pm-app/app/scripts/angular/pmsoft/config.js
@@ -12,8 +12,16 @@
 
     function PMSoftConfig($httpProvider) {
 
-        //$locationProvider.html5Mode(true);
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
+
+        // Disable IE ajax request caching start
+        if (!$httpProvider.defaults.headers.get) {
+            $httpProvider.defaults.headers.get = {};
+        }
+        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+        $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        // Disable IE ajax request caching end
+
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
     }
 
