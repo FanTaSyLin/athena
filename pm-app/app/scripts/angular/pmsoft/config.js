8c2602c309dbea52f63220de32c4d89f89fe592f
FanTaSyLin <fanlin19830908@hotmail.com>
2016-09-30 14:11:02 +0800

    放开了angular的http get缓存机制

--- pm-app/app/scripts/angular/pmsoft/config.js
+++ pm-app/app/scripts/angular/pmsoft/config.js
@@ -14,13 +14,15 @@
 
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/json; charset=utf-8';
 
-        // Disable IE ajax request caching start
+        /*
+        //Disable IE ajax request caching start
         if (!$httpProvider.defaults.headers.get) {
             $httpProvider.defaults.headers.get = {};
         }
         $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
         $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
-        // Disable IE ajax request caching end
+        //Disable IE ajax request caching end
+        */
 
         delete $httpProvider.defaults.headers.common['X-Requested-With'];
     }
