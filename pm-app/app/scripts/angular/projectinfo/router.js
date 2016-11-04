f1810666573d723a3301319c04b1b1f9563e0a6a
FanTaSyLin <fanlin19830908@hotmail.com>
2016-11-04 10:47:46 +0800

    建立ProjectInfo页面的代码结构

---
+++ pm-app/app/scripts/angular/projectinfo/router.js
@@ -0,0 +1,16 @@
+/**
+ * Created by FanTaSyLin on 2016/11/4.
+ */
+
+(function () {
+    
+    'use strict';
+    
+    angular.module('ProjectInfo')
+        .config(ProjectInfoRouter);
+    
+    function ProjectInfoRouter() {
+        
+    }
+    
+})();
---
+++ pm-app/app/scripts/angular/projectinfo/services.js
@@ -0,0 +1,18 @@
+/**
+ * Created by FanTaSyLin on 2016/11/4.
+ */
+
+(function () {
+
+    'use strict';
+
+    angular.module('ProjectInfo')
+        .factory('ProjectInfoServices', ProjectInfoServicesFn);
+
+    ProjectInfoServicesFn.$inject = ['$http'];
+
+    function ProjectInfoServicesFn($http) {
+
+    }
+
+})();
