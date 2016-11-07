6625934bf7a55406fb64b5abfb95b18435c48ac4
FanTaSyLin <fanlin19830908@hotmail.com>
2016-11-07 17:09:09 +0800

    语法检查

--- auth/auth.js
+++ auth/auth.js
@@ -53,7 +53,7 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1')
+    res.header("X-Powered-By",' 3.2.1');
     if(req.method=="OPTIONS")
         res.send(200);/*让options请求快速返回*/
     else
