377da048636f831e468a2984efab0e6056d3fd1e
FanTaSyLin <fanlin19830908@hotmail.com>
2017-01-14 10:11:49 +0800

    审核页面中左侧项目列表 现在能够自动滤除掉未审核记录为0的项目。可以有效地减少列表高度

--- pm-app/app/scripts/angular/pmsoft/filter.js
+++ pm-app/app/scripts/angular/pmsoft/filter.js
@@ -6,8 +6,12 @@
 
     'use strict';
 
-    angular.module('PMSoft')
-        .filter('to_trusted', toTrusted);
+    var app = angular.module('PMSoft');
+    app.filter('to_trusted', toTrusted);
+    /**
+     * 移除掉未审核记录数量为0的项目（审核页面左侧列表）
+     */
+    app.filter('rm_project_unaudited_count0', rmProjectByCount0);
 
     toTrusted.$inject = ['$sce'];
 
@@ -15,6 +19,18 @@
         return function (text) {
             return $sce.trustAsHtml(text);
         };
+    }
+    
+    function rmProjectByCount0() {
+        return function (items) {
+            var array = [];
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].count !== 0) {
+                    array.push(items[i]);
+                }
+            }
+            return array;
+        }
     }
 
 })();
